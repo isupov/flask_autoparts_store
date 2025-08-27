166311007d9fb75bd9e0f72925a4e47658ea8271
isupov <isupovilya@gmail.com>
2025-08-27 09:05:40 +0700

    run-app

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from flask import Flask
+from app import create_app, db
+from app.models import User, Category, Brand, Country, Product, CartItem, News, product_category
 
-app = Flask(__name__)
+app = create_app()
 
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
+# Создаем контекст приложения для shell
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        'db': db,
+        'User': User,
+        'Category': Category,
+        'Brand': Brand,
+        'Country': Country,
+        'Product': Product,
+        'CartItem': CartItem,
+        'News': News,
+        'product_category': product_category
+    }
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5002, debug=True)
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
